6e3e81748923f57b4525a8be5091404ba577416f
SaranshSaxena1 <90303481+SaranshSaxena1@users.noreply.github.com>
2023-07-04 23:27:19 +0530

    Add files via upload

---
+++ SHORTESTJOBFIRST.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+//defininf struct
+struct Process {
+    int pid;
+    int burst_time;
+    int priority;
+    int arrival_time;
+};
+
+int main() {
+    int num_processes, i, j;
+    float avg_waiting_time = 0, avg_turnaround_time = 0;
+    //taking input
+    printf("Enter the number of processes: ");
+    scanf("%d", &num_processes);
+    
+    struct Process processes[num_processes], temp;
+    
+    for (i = 0; i < num_processes; i++) {
+        processes[i].pid = i + 1;
+        //taking at bt p
+        printf("Enter the arrival time, burst time and priority for process %d: ", processes[i].pid);
+        scanf("%d %d %d", &processes[i].arrival_time, &processes[i].burst_time, &processes[i].priority);
+    }
+    //sorting table in order
+    for (i = 0; i < num_processes - 1; i++) {
+        for (j = i + 1; j < num_processes; j++) {
+            if (processes[i]./*priority*/burst_time > processes[j].burst_time/*priority*/) {
+                temp = processes[i];
+                processes[i] = processes[j];
+                processes[j] = temp;
+            }
+        }
+    }
+    
+    int waiting_time[num_processes], turnaround_time[num_processes];
+    
+    waiting_time[0] = 0;
+    
+    for (i = 1; i < num_processes; i++) {
+        int total_waiting_time = 0;
+        for (j = 0; j < i; j++) {
+            if (processes[j].arrival_time <= processes[i].arrival_time) {
+                total_waiting_time += processes[j].burst_time;
+            }
+        }
+        waiting_time[i] = total_waiting_time;
+        avg_waiting_time += waiting_time[i];
+    }
+    
+    for (i = 0; i < num_processes; i++) {
+        turnaround_time[i] = waiting_time[i] + processes[i].burst_time;
+        avg_turnaround_time += turnaround_time[i];
+    }
+    //printing the table
+    printf("\nProcess\tArrival time\tBurst time\tPriority\tWaiting time\tTurnaround time\n");
+    
+    for (i = 0; i < num_processes; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].pid, processes[i].arrival_time, processes[i].burst_time, processes[i].priority, waiting_time[i], turnaround_time[i]);
+    }
+    
+    avg_waiting_time /= num_processes;
+    avg_turnaround_time /= num_processes;
+
+    //print the result
+    
+    printf("\nAverage waiting time: %.2f", avg_waiting_time);
+    printf("\nAverage turnaround time: %.2f", avg_turnaround_time);
+    
+    return 0;//end
+}
